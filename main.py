5c6a76c21d99057bc5098002a71ecd7c9aa3d000
diego <diegoard13@gmail.com>
2021-10-09 18:57:34 -0500

    rutas

--- main.py
+++ main.py
@@ -2,60 +2,83 @@ from types import MethodDescriptorType
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask.globals import request
 from flask_mysqldb import MySQL
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+user_list = pd.DataFrame({'nombre':['diego','ethan','carolina','yendi','nellys'], 'email':['diego@email.com','ethan@email.com','carolina@email.com','yendi@email.com','nellys@email.com'], 'contraseña': ['123','234','456','789','246']})
+
+
+@app.route('/home', methods = ['POST','GET'])
 def home():
 
-    return render_template('login.html')
+    return render_template('Main.html')
 
-@app.route('/signup', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form["mail"]
+        contraseña = request.form["contraseña"]
+        if email and contraseña in user_list.values:
+            print(email,contraseña)
+            return redirect(url_for('feed'))
+        else:
+            return render_template('login.html')
+
+    return render_template('login.html')
+    
+
+@app.route('/feed', methods = ['POST','GET'])
+def feed():
+    nombre = request.form.get("nombre")
+    contraseña = request.form.get('contraseña')
+    return render_template('Feed.html')
+
+@app.route('/passwordRecovery')
+def recovery():
+    
+    return render_template('passwordRecovery.html')
+
+@app.route('/signup',methods=['GET','POST'])
 def signup():
     if request.method=='POST':
         nombre = request.form['nombre']
-        telefono = request.form['telefono']
         email = request.form['email']
-        flash('contacto agregado satisfactoriamente')
-        return redirect(url_for('Main'))
-    return 'add contact'
-
-
-@app.route('/login')
-def login(id):
-   
-    return render_template('login.html')
+        contraseña = request.form['contraseña']
+        list=[nombre,email,contraseña]
+        user_list.loc[len(user_list)] = list
+        return redirect(url_for('login'))
+    else:
+        return render_template('Signup.html')
 
 
 
-@app.route('/admin/<id>')
-def admin(id):
+
+
+
+@app.route('/admin')
+def admin():
    
     return render_template('admin.html')
 
-@app.route('/feed/<id>')
-def feed(id):
-   
-    return render_template('feed.html')
+
 
 
 #Actualizando contactos provenientes del formulario de edit_contact
-@app.route('/poster/<id>', methods = ['POST'])
-def update_contact(id):
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        telefono = request.form['telefono']
-        email = request.form['email']
-      
-        flash ('contacto actualizado')
+@app.route('/poster')
+def update_contact():
+
+
         return redirect(url_for('Main'))
 
 
 #Eliminar un registro con el ingreso de un parametro id proveniente desde index.html
-@app.route('/PubDetallada/<string:id>')
-def delete_contact(id):
+@app.route('/PubDetallada')
+def pubDetallada():
    
-    flash('Usuario eliminado')
+    
     return redirect(url_for('home'))
 
     
